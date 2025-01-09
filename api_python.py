8526add0bccb590d63a4b6ded2051553b25741c1
donaseb <37175842+donaseb@users.noreply.github.com>
2025-01-09 11:45:59 +0530

    Update api_python.py

--- api_python.py
+++ api_python.py
@@ -1,1 +1,19 @@
+import requests
+
+# Define the URL and parameters
+url = "https://api.weatherapi.com/v1/current.json"
+params = {
+    'q': 'bangalore',
+    'key': '7580f3b2ceee433bb9880636250401'
+}
+
+# Send the GET request
+response = requests.get(url, params=params)
+
+# Check if the request was successful
+if response.status_code == 200:
+    data = response.json()  # Parse the JSON response
+    print(data)
+else:
+    print(f"Error: {response.status_code}")
 
